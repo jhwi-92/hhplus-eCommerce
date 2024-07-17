7277dd7228ed406e2d3aacf3049c5af3d0a02619
jh <dwg100@nate.com>
2024-07-17 23:21:28 +0900

    query DSL 적용

--- src/main/java/hhplus/ecommoerce/biz/application/domain/repository/ProductRepositoryImpl.java
+++ src/main/java/hhplus/ecommoerce/biz/application/domain/repository/ProductRepositoryImpl.java
@@ -1,24 +1,41 @@
 package hhplus.ecommoerce.biz.application.domain.repository;
 
 
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import hhplus.ecommoerce.biz.application.domain.entity.Product;
-import java.time.LocalDateTime;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Iterator;
+import hhplus.ecommoerce.biz.application.domain.entity.QOrderHistory;
+import hhplus.ecommoerce.biz.application.domain.entity.QProduct;
 import java.util.List;
-import java.util.ListIterator;
 
+import lombok.RequiredArgsConstructor;
+
+
+@RequiredArgsConstructor
 public class ProductRepositoryImpl implements ProductRepositoryCustom {
 
 
+    private final JPAQueryFactory queryFactory;
     @Override
     public List<Product> findProductTopList() {
 
-        List<Product> expectedProducts = Arrays.asList(
-            new Product(1L, "윤용한", 1, 3700, LocalDateTime.now()),
-            new Product(2L, "아이유", 2, 980000000, LocalDateTime.now())
-        );
-        return expectedProducts;
+        QProduct product = QProduct.product;
+        QOrderHistory orderHistory = QOrderHistory.orderHistory;
+
+        // 서브쿼리를 인라인으로 작성
+        JPAQuery<Long> subQuery = queryFactory
+            .select(orderHistory.productId)
+            .from(orderHistory)
+            .where(orderHistory.status.eq("완료")
+                .and(orderHistory.yyyyMmDd.goe("20240710")))
+            .groupBy(orderHistory.productId)
+            .orderBy(orderHistory.productId.count().desc())
+            .limit(5);
+
+        // 메인 쿼리
+        return queryFactory
+            .selectFrom(product)
+            .where(product.id.in(subQuery))
+            .fetch();
     }
 }
