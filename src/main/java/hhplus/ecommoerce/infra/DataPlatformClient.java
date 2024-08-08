3c7485dbabd17d0b95fab806042c595ad555b262
jh <dwg100@nate.com>
2024-08-09 04:27:36 +0900

    데이터 플랫폼 전송을 위한 eventListener 추가

--- src/main/java/hhplus/ecommoerce/infra/DataPlatformClient.java
+++ src/main/java/hhplus/ecommoerce/infra/DataPlatformClient.java
@@ -1,11 +1,12 @@
 package hhplus.ecommoerce.infra;
 
+import hhplus.ecommoerce.biz.application.domain.entity.Payment;
 import org.springframework.stereotype.Component;
 
 @Component
 public class DataPlatformClient {
 
-    public boolean sendDataPlatform() {
+    public boolean sendDataPlatform(Payment payment) {
         return true;
     }
 
